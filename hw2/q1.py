af2bd92b6d8da8170afa44e41abe28e5e5b52561
Olcay <olcayakman@hotmail.com>
2020-12-11 12:06:30 +0300

    q1 preprocessing is completed

--- hw2/q1.py
+++ hw2/q1.py
@@ -3,6 +3,7 @@
 import PIL 
 from PIL import Image 
 import os, os.path
+import numpy as np
 
 def is_grey_scale(img_path):
     img = Image.open(img_path).convert('RGB')
@@ -15,6 +16,7 @@ def is_grey_scale(img_path):
 
 # open the image and put into list.
 imgs = []
+X = []
 path = "datasets/van_gogh"
 valid_images = [".jpg"]
 i = 1
@@ -23,12 +25,28 @@ for f in os.listdir(path):
     if ext.lower() not in valid_images:
         continue
     cur_img = Image.open(os.path.join(path,f))
+    # if len(cur_img.size) < 3:
+    #     print(i, ": ", cur_img.size)
+    cur_img_data = np.asarray(cur_img)
+    if len(cur_img_data.shape) < 3:
+        print(i, ": ", cur_img_data.shape, f)
+        # rgbimg = Image.new("RGBA", cur_img.size)
+        # rgbimg.paste(cur_img)
+        rgbimg = cur_img.convert('RGB')
+        rgbimg_data = np.asarray(rgbimg)
+        print("its shape", rgbimg_data.shape)
+        imgs.append(rgbimg_data)
+        continue
+    X.append(cur_img_data.flatten().reshape(4096, 3))
     if is_grey_scale(os.path.join(path,f)):
-        print(i, "grayscale")
-    imgs.append(cur_img)
+        print(i, "grayscale", f)
+    imgs.append(cur_img_data)
     i = i + 1
 
-print(imgs[len(imgs)-1].mode) ## making sure we got the images right.
+print(X[3].shape)
+
+##q1 preprocessing complete
+
 
 
 
