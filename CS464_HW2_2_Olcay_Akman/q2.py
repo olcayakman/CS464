0e0792dd48f673610b6bca3503d9e2ccbfdca672
Olcay <olcayakman@hotmail.com>
2020-12-12 19:40:26 +0300

    worked a little bit on q4. submitted the homework, it is not complete.

---
+++ CS464_HW2_2_Olcay_Akman/q2.py
@@ -0,0 +1,16 @@
+##begins here
+
+''' 
+    suhffle and split the data set to 5 folds
+    where each fold contains 100 samples
+'''
+
+
+'''
+    train linear regression models using the following setup:
+    seperate fold i as test and train a model on the remaining
+    folds. in the end, you should have trained 5 models, one
+    per test fold.
+    --- for each model calculate R^2, mean squared error, 
+        mean absolute error, and mean absolute percentage error.
+'''
